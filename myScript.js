ea00fc9b91414ef862a712c18545e36c8ed9b918
G0dS0n <ashek28@gmail.com>
2023-10-05 13:59:48 +1100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,15 +1,4 @@
-var path = 'https://cdn.jsdelivr.net/gh/G0dS0n/Scaffold@main/myScript.js';
-
-	(function() {
-		var script = document.createElement('script');
-		script.src = path;
-		script.async = true;
-		script.charset = "UTF-8";
-		script.onload = function() {
-			alert("Alerting");
-		};
-		var firstScript = document.getElementsByTagName("script")[0];
-	
+(function() {
 		
-		firstScript.parentNode.insertBefore(script, firstScript);
-	})();
+			alert("Alertingdd");
+})();
