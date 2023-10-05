2305ef5d6d9c1f5b6ca7225415fdf96fc8e02156
G0dS0n <ashek28@gmail.com>
2023-10-05 14:12:53 +1100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,4 +1,7 @@
-
-		
-			alert("Alertingdd");
-
+var link = document.querySelector(".customAlertLink");
+        if (link) {
+            link.addEventListener("click", function(event) {
+                event.preventDefault();  // Prevent the default action (navigation) of the link
+                alert("Alerting");
+            });
+        }
