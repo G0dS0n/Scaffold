32fe65c66836f3fd25193e2993f2382cceebf751
G0dS0n <ashek28@gmail.com>
2023-10-11 11:16:11 +1100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,4 +1,10 @@
-
-   
-            alert('Hello from GitHub-hosted JS!");
-       
+document.addEventListener("DOMContentLoaded", function() {
+    var link = document.querySelector(".customAlertLink");
+    
+    if (link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            alert("This alert is from the script hosted on GitHub!");
+        });
+    }
+});
