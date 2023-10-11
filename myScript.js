1b505be6254b71dc204ff198f95411768ff0f4c1
G0dS0n <ashek28@gmail.com>
2023-10-11 11:05:01 +1100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,9 +1,4 @@
 
-    var linkElement = document.querySelector(".customAlertLink");
-    if (linkElement) {
-        linkElement.addEventListener("click", function(event) {
-            event.preventDefault();
-            alert('Hello from GitHub-hosted JS! This is after commit');
-        });
-    }
-
+   
+            alert('Hello from GitHub-hosted JS!");
+       
